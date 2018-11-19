05ebe1dcc9907a44798776fb736be8cbc141a324
KordianD <kordiandrozd@gmail.com>
2018-11-19 19:30:29 +0100

    Correct setup.py to be aligned with PEP-8

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name = 'filepy',
-    packages = ['filepy'],
-    version = '0.1', 
-    description = 'File converter',
-    author = 'KordianD',
-    author_email = 'not_valid@gmail.com',
-    url = 'https://github.com/KordianD/filepy',
-    keywords = ['file', 'convert', 'extensions', 'csv', 'arff'],
-    classifiers = ['Programming Language :: Python :: 3', 
-                   'License :: OSI Approved :: MIT License']
+    name='filepy',
+    packages=['filepy'],
+    version='0.1',
+    description='File converter',
+    author='KordianD',
+    author_email='not_valid@gmail.com',
+    url='https://github.com/KordianD/filepy',
+    keywords=['file', 'convert', 'extensions', 'csv', 'arff'],
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License']
 )

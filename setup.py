f0c7f05cb76513883037f9e4fa721b618e1e46e7
KordianD <kordiandrozd@gmail.com>
2018-11-19 17:48:54 +0100

    Adding configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'filepy',
+    packages = ['filepy'],
+    version = '0.1', 
+    description = 'File converter',
+    author = '',
+    author_email = '',
+    url = '',
+    download_url = 'download link you saved',
+    keywords = ['file', 'convert', 'extensions', 'csv', 'arff'],
+)

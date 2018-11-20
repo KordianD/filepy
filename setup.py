1f2c3cd817e26c2c2a8fb07c1b61ef0e2df4f8b7
KordianD <kordiandrozd@gmail.com>
2018-11-20 22:16:29 +0100

    Modified version setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(
     install_requires=['numpy'],
     long_description=open('README.md').read(),
     classifiers=['Programming Language :: Python :: 3',
-                 'License :: OSI Approved :: MIT License']
+                 'License :: OSI Approved :: MIT License'],
+    long_description_content_type='text/markdown'
 )

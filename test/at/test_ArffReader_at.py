6e5156b44245cb6f731cd8c8bdfa51eab7969470
KordianD <kordiandrozd@gmail.com>
2018-11-20 19:36:53 +0100

    Added acceptance test for ArffReader with data file

---
+++ test/at/test_ArffReader_at.py
@@ -0,0 +1,12 @@
+from filepy.ArffReader import ArffReader
+
+TEST_FILENAME = 'data/arff_files/with_matrix_10_x_10_and_column_names.arff'
+
+
+def test_correctly_read_matrix_with_column_labels():
+    arff_reader = ArffReader(TEST_FILENAME)
+    arff_reader.analyse()
+    assert arff_reader.relation == 'iris'
+    assert arff_reader.attributes == [('sepallength', 'NUMERIC'), ('sepalwidth', 'NUMERIC'), ('petallength', 'NUMERIC'),
+                                      ('petalwidth', 'NUMERIC'),
+                                      ('class', '{Iris-setosa,Iris-versicolor,Iris-virginica}')]

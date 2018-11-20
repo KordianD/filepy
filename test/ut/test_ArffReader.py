5fa1c1601e918d829c186641919f56cdc0dc16c8
KordianD <kordiandrozd@gmail.com>
2018-11-20 21:18:20 +0100

    Removed unneccessary fixture and use simple unit test basic functions

--- test/ut/test_ArffReader.py
+++ test/ut/test_ArffReader.py
@@ -1,21 +1,12 @@
-import pytest
-
 from filepy.ArffReader import ArffReader
 
-TEST_FILENAME = 'test'
 
-
-@pytest.fixture
-def arff_reader():
-    return ArffReader(TEST_FILENAME)
-
-
-def test_when_line_contains_data_declaration_is_line_containing_declaration_should_return_true(arff_reader):
+def test_when_line_contains_data_declaration_is_line_containing_declaration_should_return_true():
     line_with_data_declaration = '@data'
-    assert arff_reader.is_line_containing_declaration(
+    assert ArffReader.is_line_containing_declaration(
         line_with_data_declaration, 'data')
 
 
-def test_when_line_is_empty_is_line_containing_declaration_should_return_false(arff_reader):
+def test_when_line_is_empty_is_line_containing_declaration_should_return_false():
     empty_line = ""
-    assert not arff_reader.is_line_containing_declaration(empty_line, 'data')
+    assert not ArffReader.is_line_containing_declaration(empty_line, 'data')

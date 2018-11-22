cdc19babd3692363cb23e3513b45554fb25a3a75
KordianD <kordiandrozd@gmail.com>
2018-11-22 19:56:15 +0100

    Added DTO object and part of ArffWriter

--- filepy/ArffWriter.py
+++ filepy/ArffWriter.py
@@ -1,6 +1,26 @@
 class ArffWriter:
-    def __init__(self, path_to_file):
+    def __init__(self, path_to_file, delimiter=','):
         self.path_to_file = path_to_file
+        self.delimiter = delimiter
+        self.dto = None
 
     def write(self):
+        with open(self.path_to_file, 'w') as file:
+            self._write_relation_to_file(file)
+            self._write_labels_to_file(file)
+
+    def _write_relation_to_file(self, file):
+        file.write(r'@relation {}'.format(self._extract_filename_from_path_to_file()))
+
+    def _extract_filename_from_path_to_file(self):
+        index_of_starting_filename = self.path_to_file.rfind('/')
+        if index_of_starting_filename == -1:
+            return self.path_to_file
+        return self.path_to_file[index_of_starting_filename + 1:]
+
+    def _write_labels_to_file(self, file):
         pass
+
+    def _write_data_to_file(self, file):
+        for row in self.dto:
+            file.write(",".join(row) + '\n')
---
+++ filepy/DTO.py
@@ -0,0 +1,4 @@
+class DTO:
+    def __init__(self, data, columns):
+        self.data = data
+        self.columns = columns
